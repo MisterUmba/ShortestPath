bd77f11f3b7e2a2048f3027a0bcbf1602df0c898
MisterUmba <umbamw@gmail.com>
2020-04-16 08:51:41 -0400

    Kruskal's algorithm now works perfectly fine

--- Algorithms.js
+++ Algorithms.js
@@ -2,7 +2,7 @@
 // Highlight the edges from the node to the source node
 function highlightPath(node) {
     let ed = undefined;
-    if(node.path.dist === Infinity){
+    if (node.path.dist === Infinity) {
         alert("There is no path to that node");
         return;
     }
@@ -44,7 +44,7 @@ function minUnvisited(queue) {
 }
 
 // Resets the graph's weighs 
-function resetGraph(src, Q){
+function resetGraph(src, Q) {
     for (let k = 0; k < Graph.length; k++) {
         Graph[k].path = { dist: Infinity, last: undefined };
         Q.push(Graph[k]);
@@ -56,7 +56,7 @@ function resetGraph(src, Q){
 function dijkstra(src) {
     let Q = [];
     let S = [];
-    
+
     resetGraph(src, Q);
 
     while (Q.length !== 0) {
@@ -81,34 +81,34 @@ function dijkstra(src) {
 // A* shortest path algorithm
 function astar(src, goal) {
     console.log(src);
-    if(goal === undefined) return;
+    if (goal === undefined) return;
     let open_list = []
     let close_list = []
-    
+
     for (let k = 0; k < Graph.length; k++) {
-        Graph[k].path = {gScore: Infinity, dist: Infinity, last: undefined };
+        Graph[k].path = { gScore: Infinity, dist: Infinity, last: undefined };
     }
-    src.path = {gScore: 0, dist: 0, last: src };
+    src.path = { gScore: 0, dist: 0, last: src };
 
     open_list.push(src);
 
-    while(open_list.length !== 0 ){
+    while (open_list.length !== 0) {
         let curr_node = minUnvisited(open_list);
         console.log(curr_node, Graph.indexOf(curr_node));
         close_list.push(curr_node);
-        
-        if(curr_node === goal){
+
+        if (curr_node === goal) {
             break;
         }
-        
-        for(let x = 0; x < curr_node.Edges.length; x++){
+
+        for (let x = 0; x < curr_node.Edges.length; x++) {
             let score = curr_node.path.gScore + curr_node.Edges[x].cost;
-            if(score < curr_node.Edges[x].e.path.gScore){
+            if (score < curr_node.Edges[x].e.path.gScore) {
                 curr_node.Edges[x].e.path.last = curr_node;
                 curr_node.Edges[x].e.path.gScore = score;
                 curr_node.Edges[x].e.path.dist = score + distance(curr_node, goal);
-                
-                if(!close_list.includes(curr_node.Edges[x].e)){
+
+                if (!close_list.includes(curr_node.Edges[x].e)) {
                     open_list.push(curr_node.Edges[x].e);
                 }
             }
@@ -117,31 +117,31 @@ function astar(src, goal) {
     }
 
     let temp = "";
-    for(let x = 0; x < Graph.length; x++) temp += `${x} --> ${Graph.indexOf(Graph[x].path.last)} \n`;
+    for (let x = 0; x < Graph.length; x++) temp += `${x} --> ${Graph.indexOf(Graph[x].path.last)} \n`;
     //console.log(temp);
 }
 
 // Bellman-ford algorithm 
-function bellmanford(src){
-    for(let x = 0; x < Graph.length; x++){
-        Graph[x].path = {dist: Infinity, last: undefined};
+function bellmanford(src) {
+    for (let x = 0; x < Graph.length; x++) {
+        Graph[x].path = { dist: Infinity, last: undefined };
     }
 
-    src.path = {dist: 0, last: src};
+    src.path = { dist: 0, last: src };
 
     let edge_list = [];
 
-    for(let x = 0; x < Graph.length; x++){
-        for(let i = 0; i < Graph[x].Edges.length; i++){
+    for (let x = 0; x < Graph.length; x++) {
+        for (let i = 0; i < Graph[x].Edges.length; i++) {
             edge_list.push(Graph[x].Edges[i])
         }
     }
 
     console.log(`Size of Edge list: ${edge_list.length}`)
 
-    for(let i = 1; i < Graph.length; i ++){
-        for(let x = 0; x < edge_list.length; x++){
-            if(edge_list[x].b.path.dist + edge_list[x].cost < edge_list[x].e.path.dist){
+    for (let i = 1; i < Graph.length; i++) {
+        for (let x = 0; x < edge_list.length; x++) {
+            if (edge_list[x].b.path.dist + edge_list[x].cost < edge_list[x].e.path.dist) {
                 console.log(`${edge_list[x].b.path.dist + edge_list[x].cost} < ${edge_list[x].e.path.dist}`);
                 edge_list[x].e.path.dist = edge_list[x].b.path.dist + edge_list[x].cost;
                 edge_list[x].e.path.last = edge_list[x].b;
@@ -150,18 +150,45 @@ function bellmanford(src){
     }
 
     let temp = "";
-    for(let x = 0; x < Graph.length; x++) temp += `${x} --> ${Graph.indexOf(Graph[x].path.last)} \n`;
+    for (let x = 0; x < Graph.length; x++) temp += `${x} --> ${Graph.indexOf(Graph[x].path.last)} \n`;
     console.log(temp);
 }
 
 
 // Prim's MST algorithm 
-function prim(src){
-    
+function prim(src) {
+
 }
 
+function findParent(v){
+    let parent = Graph.indexOf(v.path.last);
+    while(true){
+        if(Graph[parent].path.last === Graph[parent])
+            return Graph[parent];
+        parent = Graph.indexOf(Graph[parent].path.last)
+    }
+}
 
-// kruskel's MST algorithm 
-function kruskel(src){
-    
+// kruskal's MST algorithm 
+function kruskal(src) {
+
+    let edge_list = [];
+
+    for(let x = 0; x < Graph.length; x ++){
+        Graph[x].path = {dist: 0, last: Graph[x]};
+        for(let y = 0; y < Graph[x].Edges.length; y ++){
+            Graph[x].Edges[y].path = {dist : Graph[x].Edges[y].cost, last : Graph[x].Edges[y] };
+            edge_list.push(Graph[x].Edges[y]);
+        }
+    }
+
+    let count = 0;
+    while(count < Graph.length - 1){
+        let curr_edge = minUnvisited(edge_list);
+        if(findParent(curr_edge.b) !== findParent(curr_edge.e)){
+            curr_edge.e.path.last = curr_edge.b;
+            curr_edge.selected = true;
+            count++;
+        }
+    }
 }
